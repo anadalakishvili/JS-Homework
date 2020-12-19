9ccb7356f21fc67c4b2b9462de4c4a9e82333eda
anadalakishvili <73490309+anadalakishvili@users.noreply.github.com>
2020-12-19 22:47:23 +0400

    Add files via upload

---
+++ closure.js
@@ -0,0 +1,14 @@
+/*http://nicholasjohnson.com/javascript/javascript-for-programmers/exercises/closure/ */
+ Guess the output
+1
+ //Output  is going to be 12
+2
+//Output  is going to be 12
+3
+//Output  is going to be 12
+4
+//Output  is going to be 10
+5
+//Output  is going to be 12
+6
+//Output  is going to be 15
---
+++ jshero.js
@@ -0,0 +1,87 @@
+//1.
+let firstname="Lata";
+//2.
+'Geeta'
+//3.
+let flower="rose"
+let tree="maple"
+//4.
+'Toe'
+//5.
+'Hardy'
+//6.
+function hello() {
+    return 'hello world!';
+  }
+//7.
+function a() {
+    return 'Hello a!';
+  }
+  
+  function b() {
+    return 'Hello b!';
+  }
+
+  //8.
+  function greet() {
+    return 'Haydo!';
+  }
+  
+  let salutation = greet();
+  
+  //9.
+   Hi!
+//10.
+function echo(sound) {
+    return sound;
+  }
+//11.
+'How do you do?'
+//12.
+function greet(name) {
+    return 'Hello ' + name + '!';
+  }
+//13.
+'Where is Jacky?'
+//14.
+'Hi Selva! Hi Pola!'
+//15.
+function log() {
+    console.log('Hello Console!');
+  }
+//16.
+function log(name) {
+    console.log(name);
+  }
+//17.
+function shout(fire) {
+    let result = fire + fire;
+    console.log(result);
+    return result;
+  }
+//18.
+'Roy and Roy'
+//19.
+function length(x) {
+  return x.length;
+}
+//20
+function toCase(word) {
+    return word.toLowerCase() + '-' + word.toUpperCase();
+  }
+//21
+function shortcut(x, y) {
+    return x.charAt(0) + y.charAt(0);
+  }
+//22
+function firstChar(x) {
+    let trimmedText = x.trim();
+    return trimmedText.charAt(0);
+  }
+//23
+function indexOfIgnoreCase(x, y) {
+    const x_1 = x.toLowerCase();
+    const y_1 = y.toLowerCase();
+    return x_1.indexOf(y_1);
+  }
+//24
